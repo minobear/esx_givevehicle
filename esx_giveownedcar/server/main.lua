f7e78faece6749c7bf5ebff1036c45687bb20b7a
MINO <55931955+minobear@users.noreply.github.com>
2020-11-06 11:13:09 +0800

    Update main.lua

--- esx_giveownedcar/server/main.lua
+++ esx_giveownedcar/server/main.lua
@@ -2,29 +2,32 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 --give car with a random plate- 1: playerID 2: carModel (3: plate)
-TriggerEvent('es:addGroupCommand', 'givecar', 'admin', function(source, args, user)
-	if args[1] == nil or args[2] == nil then
-		TriggerClientEvent('esx:showNotification', source, '~r~/givecar [playerID] [carModel] <plate>')
-	elseif args[3] ~= nil then
-		local playerName = GetPlayerName(args[1])
-		local plate = args[3]
-		if #args > 3 then
-			for i=4, #args do
-				plate = plate.." "..args[i]
-			end		
-		end	
-		plate = string.upper(plate)
-		TriggerClientEvent('esx_giveownedcar:spawnVehiclePlate', source, args[1], args[2], plate, playerName, 'player')		
+RegisterCommand('givecar', function(source, args)
+	local identifier = ESX.GetPlayerFromId(source).identifier
+	if havePermission(identifier)
+		if args[1] == nil or args[2] == nil then
+			TriggerClientEvent('esx:showNotification', source, '~r~/givecar [playerID] [carModel] <plate>')
+		elseif args[3] ~= nil then
+			local playerName = GetPlayerName(args[1])
+			local plate = args[3]
+			if #args > 3 then
+				for i=4, #args do
+					plate = plate.." "..args[i]
+				end		
+			end	
+			plate = string.upper(plate)
+			TriggerClientEvent('esx_giveownedcar:spawnVehiclePlate', source, args[1], args[2], plate, playerName, 'player')		
+		else
+			local playerName = GetPlayerName(args[1])
+			TriggerClientEvent('esx_giveownedcar:spawnVehicle', source, args[1], args[2], playerName, 'player')
+		end
 	else
-		local playerName = GetPlayerName(args[1])
-		TriggerClientEvent('esx_giveownedcar:spawnVehicle', source, args[1], args[2], playerName, 'player')
+		TriggerClientEvent('esx:showNotification', source, '~r~You don\'t have permission to execute this command!')
 	end
-end, function(source, args, user)
-	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
-end, {help = 'Give a car to the target player', params = {{name = "id", help = 'The ID of player'}, {name = "vehicle", help = 'Name of the car model'}, {name = "<plate>", help = 'Custom plate name, if none will randomly generate a new plate'}}})
+end)
 
 RegisterCommand('_givecar', function(source, args)
-    if source == 0 then	
+    if source == 0 then
 		local sourceID = args[1]
 		if args[1] == nil or args[2] == nil then
 			print("SYNTAX ERROR: _givecar [playerID] [carModel] <plate>")
@@ -45,30 +48,33 @@ RegisterCommand('_givecar', function(source, args)
 	end
 end)
 
-TriggerEvent('es:addGroupCommand', 'delcarplate', 'admin', function(source, args, user)	
-	if args[1] == nil then
-		TriggerClientEvent('esx:showNotification', source, '~r~/delcarplate [plate]')
-	else
-		local plate = args[1]
-		if #args > 1 then
-			for i=2, #args do
-				plate = plate.." "..args[i]
+RegisterCommand('delcarplate', function(source, args)
+	local identifier = ESX.GetPlayerFromId(source).identifier
+	if havePermission(identifier)
+		if args[1] == nil then
+			TriggerClientEvent('esx:showNotification', source, '~r~/delcarplate [plate]')
+		else
+			local plate = args[1]
+			if #args > 1 then
+				for i=2, #args do
+					plate = plate.." "..args[i]
+				end		
+			end
+			plate = string.upper(plate)
+			
+			local result = MySQL.Sync.execute('DELETE FROM owned_vehicles WHERE plate = @plate', {
+				['@plate'] = plate
+			})
+			if result == 1 then
+				TriggerClientEvent('esx:showNotification', source, _U('del_car', plate))
+			elseif result == 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('del_car_error', plate))
 			end		
 		end
-		plate = string.upper(plate)
-		
-		local result = MySQL.Sync.execute('DELETE FROM owned_vehicles WHERE plate = @plate', {
-			['@plate'] = plate
-		})
-		if result == 1 then
-			TriggerClientEvent('esx:showNotification', source, _U('del_car', plate))
-		elseif result == 0 then
-			TriggerClientEvent('esx:showNotification', source, _U('del_car_error', plate))
-		end		
-	end
-end, function(source, args, user)
-	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
-end, {help = 'Delete a owned car by plate', params = {{name = "plate", help = 'The car plate'}}})
+	else
+		TriggerClientEvent('esx:showNotification', source, '~r~You don\'t have permission to execute this command!')
+	end		
+end)
 
 RegisterCommand('_delcarplate', function(source, args)
     if source == 0 then
@@ -120,3 +126,15 @@ RegisterServerEvent('esx_giveownedcar:printToConsole')
 AddEventHandler('esx_giveownedcar:printToConsole', function(msg)
 	print(msg)
 end)
+
+function havePermission(identifier)
+	local isAdmin = false
+	for _,v in pairs(Config.Admins) do
+		if v == identifier then
+			isAdmin = true
+			break
+		end
+	end
+	
+	return isAdmin
+end
