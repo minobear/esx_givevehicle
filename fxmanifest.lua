08a571a946f56f5b1dad3bdb5808faff46602cc8
DROY64 <96751323+DROY64@users.noreply.github.com>
2023-08-26 23:02:44 +0200

    Fix for it to recognise ESX

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,26 +2,23 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'MEENO'
-description 'made by MEENO'
+description 'made by MEENO, Updated by DROY64'
 
 version '1.0'
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+shared_scripts { 
+    '@es_extended/imports.lua',
     '@es_extended/locale.lua',
-    'server/main.lua',
-    'config.lua',
-    'locales/de.lua',
-    'locales/tw.lua',
-    'locales/en.lua'
+    'locales/*.lua',
+    'config.lua'
 }
 
 client_scripts {
-    '@es_extended/locale.lua',
-    'client/main.lua',
-    'config.lua',
-    'locales/de.lua',
-    'locales/tw.lua',
-    'locales/en.lua'
+    'client/main.lua'
 }
 
 dependency {
